9b8b73640138cb79e61c151f50957d1d3d8fc226
Kajus CC <42713684+KajusC@users.noreply.github.com>
2025-04-03 21:19:57 +0300

    Add project files.

---
+++ EventPlus.Server/Program.cs
@@ -0,0 +1,43 @@
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace EventPlus.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            Console.WriteLine(app.Environment.IsDevelopment());
+
+            if (builder.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = string.Empty;
+                });
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            app.MapControllers();
+            app.MapFallbackToFile("/index.html");
+
+            app.Run();
+        }
+    }
+}
+
