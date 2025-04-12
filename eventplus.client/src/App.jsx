6a96d1cd68d6f0c14b3b290acf102681649e72aa
ArnasBraskus <arnbra1@ktu.lt>
2025-04-12 20:07:33 +0300

    CRUD front end implemented

--- eventplus.client/src/App.jsx
+++ eventplus.client/src/App.jsx
@@ -1,15 +1,21 @@
-import { useEffect, useState } from 'react';
+//import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import EventList from './EventList';
+import EventView from './EventView';
+import EventEdit from './EventEdit';
+import EventInsert from './EventInsert';
 import './App.css';
 
-function App() {
-    const [forecasts, setForecasts] = useState();
+function WeatherForecast() {
+    //const [forecasts, setForecasts] = useState();
+    const navigate = useNavigate();
 
-    useEffect(() => {
+  /*  useEffect(() => {
         populateWeatherData();
     }, []);
 
     const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
+        ? <p><em>Loading... Please refresh once the ASP.NET backend has started.</em></p>
         : <table className="table table-striped" aria-labelledby="tableLabel">
             <thead>
                 <tr>
@@ -29,21 +35,35 @@ function App() {
                     </tr>
                 )}
             </tbody>
-        </table>;
+        </table>;*/
 
     return (
         <div>
-            <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
+            <h1 id="tableLabel">EventPlus</h1>
+            {/*{contents}*/}
+            <button onClick={() => navigate('/events')}>Go to Event List</button>
         </div>
     );
-    
-    async function populateWeatherData() {
+
+/*    async function populateWeatherData() {
         const response = await fetch('weatherforecast');
         const data = await response.json();
         setForecasts(data);
-    }
+    }*/
+}
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<WeatherForecast />} />
+                <Route path="/events" element={<EventList />} />
+                <Route path="/eventview/:id" element={<EventView />} />
+                <Route path="/eventedit/:id" element={<EventEdit />} />
+                <Route path="/eventinsert" element={<EventInsert />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ eventplus.client/src/EventList.jsx
@@ -0,0 +1,50 @@
+﻿import { Link, useNavigate } from 'react-router-dom';
+import './App.css';
+
+function EventList() {
+    // Hardcodinti eventai
+    const events = [
+/*        { id: 1, name: 'Concert in the Wild', ticketsAvailable: 100 },*/
+        { id: 1, name: 'Art Gallery Opening', ticketsAvailable: 50 },
+        { id: 2, name: 'Tech Conference 2025', ticketsAvailable: 200 },
+        { id: 3, name: 'Theater Play: Hamlet', ticketsAvailable: 80 },
+        { id: 4, name: 'Lady Gaga Kaunas', ticketsAvailable: 1000}
+    ];
+
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <h1 id="tableLabel">Event List</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Event Name</th>
+                        <th>Tickets Available</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {events.map(event => (
+                        <tr key={event.id}>
+                            <td>{event.name}</td>
+                            <td>{event.ticketsAvailable}</td>
+                            <td>
+                                <Link to={`/eventview/${event.id}`}>
+                                    <button>View</button>
+                                </Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <Link to={`/eventinsert`}>
+                <button>Insert</button>
+            </Link>
+            <span style={{ margin: '0 10px' }}></span>
+            <button onClick={() => navigate(-1)}>Return</button>
+        </div>
+    );
+}
+
+export default EventList;
